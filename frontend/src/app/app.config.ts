8e588f690f49df12d6c8bd4b10ae3452e0ec5491
Hemant Rajpoot <hemantrajpoot56@gmail.com>
2025-07-19 17:34:42 +0530

    Fix auth interceptor to use new token method

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,19 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './interceptors/auth.interceptor';
+import { provideHttpClient, withInterceptors, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
     provideAnimations(),
-    provideHttpClient(
-      withFetch(),
-      withInterceptors([authInterceptor])
-    )
+    provideHttpClient(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ]
 };
